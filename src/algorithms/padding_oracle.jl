7cbcda2aadcb7e2a41f01ce406fd2879ea188d88
M S <scherzma@protonmail.com>
2024-11-14 19:31:59 +0100

    added to padding oracle

--- src/algorithms/padding_oracle.jl
+++ src/algorithms/padding_oracle.jl
@@ -19,22 +19,30 @@ function send_to_server(a::PaddingClient, data::Array{UInt8})
 end
 
 
-function generate_ivs(num_ivs::UInt8, plaintext::Array{UInt8})
-    block_size = 16
-        ivs = Array{UInt8}(undef, num_ivs * block_size)
-    
-    for i in 0:num_ivs-1
-        block = zeros(UInt8, block_size)
-        counter_bytes = reinterpret(UInt8, [UInt32(i)])
-        start_idx = block_size - length(counter_bytes) + 1
-        block[1:start_idx-1] .= 0x00  # Pad with zeros
-        block[start_idx:end] .= counter_bytes
-        start_pos = (i * block_size) + 1
-        ivs[start_pos:start_pos+block_size-1] = block
+function generate_ivs(padding_length::Int, decrypted_bytes::Array{UInt8}, original_iv::Array{UInt8}=zeros(UInt8, 16))
+    ivs = UInt8[]  # Initialize an empty array to hold the concatenated IVs
+
+    for i in 0:255
+        # Copy the original IV to modify it for each attempt
+        iv = copy(original_iv)
+
+        # Adjust the known decrypted bytes to conform to the current padding length
+        for j in 1:(padding_length - 1)
+            position = 16 - (j - 1)
+            iv[position] = iv[position] ⊻ decrypted_bytes[end - (j - 1)] ⊻ UInt8(padding_length)
+        end
+
+        # Modify the byte we're currently attacking
+        position = 16 - (padding_length - 1)
+        iv[position] = iv[position] ⊻ UInt8(i) ⊻ UInt8(padding_length)
+
+        # Append the modified IV to the list
+        append!(ivs, iv)
     end
-    
+
     return ivs
 end
+
 
 function attack_block(client::PaddingClient, block::Array{UInt8})
     write(client.connection, block)
