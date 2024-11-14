6442b132569f821b9d80b15e246df1df4315b1ba
user <user@user>
2024-11-14 11:34:35 +0100

    changed sea a bit to reduce redundancy; potentially improved gfmul, by branchless programming (not sure if its really faster)

--- src/algorithms/sea128.jl
+++ src/algorithms/sea128.jl
@@ -4,20 +4,7 @@ using Nettle
 
 const SEA_CONST = Array{UInt8}([0xc0, 0xff, 0xee, 0xc0, 0xff, 0xee, 0xc0, 0xff, 0xee, 0xc0, 0xff, 0xee, 0xc0, 0xff, 0xee, 0x11])
 
-function encrypt_sea(key::Array{UInt8}, input::Array{UInt8})
-    return encrypt("AES128", key, input) .⊻ SEA_CONST
-end
-
-function encrypt_sea(algorithm::String, key::Array{UInt8}, input::Array{UInt8})
-    return encrypt(algorithm, key, input) .⊻ SEA_CONST
-end
-
-function decrypt_sea(key::Array{UInt8}, input::Array{UInt8})
-    return decrypt("AES128", key, input .⊻ SEA_CONST)
-end
-
-function decrypt_sea(algorithm::String, key::Array{UInt8}, input::Array{UInt8})
-    return decrypt(algorithm, key, input .⊻ SEA_CONST)
-end
+encrypt_sea(algorithm::String, key::Array{UInt8}, input::Array{UInt8}) = encrypt(algorithm, key, input) .⊻ SEA_CONST
+decrypt_sea(algorithm::String, key::Array{UInt8}, input::Array{UInt8}) = decrypt(algorithm, key, input .⊻ SEA_CONST)
 
 end
