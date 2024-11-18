2b0d1d7d34b55345cf8f44a845c8e00151938b5c
M S <scherzma@protonmail.com>
2024-11-18 13:18:40 +0100

    Some release changes

--- src/util/processor.jl
+++ src/util/processor.jl
@@ -145,9 +145,10 @@ function padding_oracle(jsonContent::Dict)
     port::Int = jsonContent["port"]
     iv::Array{UInt8} = base64decode(jsonContent["iv"])
     ciphertext::Array{UInt8} = base64decode(jsonContent["ciphertext"])
-    #result = padding_attack(hostname, port, iv, ciphertext)
-    result = "plaintext"
-    return base64encode(result)
+    result = padding_attack(hostname, port, iv, ciphertext)
+    #result = "plaintext"
+    #return base64encode(result)
+    return result
 end
 
 function polynomial_add(jsonContent::Dict)
