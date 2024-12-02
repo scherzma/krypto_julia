2a1b0d5e5bbc259ead2c2e1a13e02e8916057fe2
M S <scherzma@protonmail.com>
2024-12-02 12:50:28 +0100

    make stuff

---
+++ building/CMakeFiles/KryptoCpp.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/ddf.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/ddf.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/ddf.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/edf.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/edf.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/edf.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/gcm.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/gcm.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/gcm.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/padding_oracle.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/padding_oracle.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/padding_oracle.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/sea128.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/sea128.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/sea128.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/sff.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/sff.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/sff.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/algorithms/xex_fde.cpp" "CMakeFiles/KryptoCpp.dir/src/algorithms/xex_fde.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/algorithms/xex_fde.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/main.cpp" "CMakeFiles/KryptoCpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/main.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/math/galois.cpp" "CMakeFiles/KryptoCpp.dir/src/math/galois.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/math/galois.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/math/polynom.cpp" "CMakeFiles/KryptoCpp.dir/src/math/polynom.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/math/polynom.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/util/base64_utils.cpp" "CMakeFiles/KryptoCpp.dir/src/util/base64_utils.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/util/base64_utils.cpp.o.d"
+  "/home/user/CLionProjects/krypto_julia/src/util/processor.cpp" "CMakeFiles/KryptoCpp.dir/src/util/processor.cpp.o" "gcc" "CMakeFiles/KryptoCpp.dir/src/util/processor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ building/CMakeFiles/KryptoCpp.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/ddf.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/ddf.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/edf.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/edf.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/gcm.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/gcm.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/padding_oracle.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/padding_oracle.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/sea128.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/sea128.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/sff.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/sff.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/xex_fde.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/algorithms/xex_fde.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/main.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/main.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/math/galois.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/math/galois.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/math/polynom.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/math/polynom.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/util/base64_utils.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/util/base64_utils.cpp.o.d"
+  "CMakeFiles/KryptoCpp.dir/src/util/processor.cpp.o"
+  "CMakeFiles/KryptoCpp.dir/src/util/processor.cpp.o.d"
+  "KryptoCpp"
+  "KryptoCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KryptoCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
